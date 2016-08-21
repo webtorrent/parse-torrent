beada400fa19873ece6c403a556161f3c27bb911
Luca Matteis <lmatteis@gmail.com>
2016-08-21 16:31:06 +0200

    Added capability for .remote() to contact the DHT.

--- test/basic-node.js
+++ test/basic-node.js
@@ -1,6 +1,9 @@
 var fixtures = require('webtorrent-fixtures')
 var http = require('http')
+var DHT = require('bittorrent-dht')
+var ed = require('ed25519-supercop')
 var parseTorrent = require('../')
+var crypto = require('crypto')
 var test = require('tape')
 
 test('http url to a torrent file, string', function (t) {
@@ -28,5 +31,53 @@ test('filesystem path to a torrent file, string', function (t) {
   parseTorrent.remote(fixtures.leaves.torrentPath, function (err, parsedTorrent) {
     t.error(err)
     t.deepEqual(parsedTorrent, fixtures.leaves.parsedTorrent)
+  })
+})
+
+test('dht put/get of torrent (BEP46)', function (t) {
+  t.plan(4)
+
+  var infoHashBuf = new Buffer(fixtures.numbers.parsedTorrent.infoHash, 'hex')
+  t.equal(infoHashBuf.length, 20, 'infoHashBuf is 20 bytes')
+
+  var keypair = ed.createKeyPair(ed.createSeed())
+
+  var dht = new DHT({ bootstrap: false, verify: ed.verify })
+  t.once('end', function () {
+    dht.destroy()
+  })
+
+  dht.on('warning', function (err) { t.fail(err) })
+  dht.on('error', function (err) { t.fail(err) })
+
+  dht.on('ready', function () {
+    var opts = {
+      k: keypair.publicKey,
+      sign: function (buf) {
+        return ed.sign(buf, keypair.publicKey, keypair.secretKey)
+      },
+      seq: 0,
+      v: {
+        ih: infoHashBuf
+      }
+    }
+
+    var expectedHash = crypto.createHash('sha1').update(opts.k).digest()
+
+    dht.put(opts, function (_, hash) {
+      t.equal(
+        hash.toString('hex'),
+        expectedHash.toString('hex'),
+        'hash of the public key'
+      )
+
+      // should perform a dht.get
+      parseTorrent.remote('magnet:?xs=urn:btpk:' + keypair.publicKey.toString('hex'), function (err, parsedTorrent) {
+        t.ifError(err)
+        t.equal(infoHashBuf.toString('hex'), parsedTorrent.infoHash,
+          'got back what we put in'
+        )
+      }, dht)
+    })
   })
 })
